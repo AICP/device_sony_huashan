83c1748a8fcfd21f7f33d5000237e971e463fc25
AdrianDC <radian.dc@gmail.com>
2016-05-28 03:52:57 +0200

    huashan: Restructure the device tree into fragments
     * Explode all configurations and makefiles
        into feature-related files automatically imported
    
     * Based upon the fragmented structure introduced
        in android_device_nextbit_ether by Steve Kondik
    
     * Adapted for AICP due to ro.device.* in device.mk
    
    Change-Id: I952bc49056108eb912bc7b0b7366ba2122632611
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ product/device.mk
@@ -0,0 +1,12 @@
+# Glove mode
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
+# Device properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.device.chipset=Qualcomm MSM8960T Snapdragon S4 Pro \
+    ro.device.cpu=Dual-core 1.7 GHz Krait \
+    ro.device.gpu=Qualcomm Adreno 320 \
+    ro.device.rear_cam=8 MP \
+    ro.device.front_cam=VGA \
+    ro.device.screen_res=720X1280
