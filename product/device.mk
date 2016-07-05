68b74b283a91875f6b6161353f42a0b5ee9e625c
Adrian DC <radian.dc@gmail.com>
2016-07-05 20:00:14 +0200

    huashan: Fix and cleanup device hardware informations
     * Hardware informations details in AICP Settings got broken
        by the evolution to the fragmented structure,
        fix the strings as they do contain spaces
    
     * Also move the AICP specific changes to aicp.mk
        to minimize differences with CM tree and ease updates
    
     * Thanks to Trafalgar-Square for reporting it
    
    Change-Id: I2308560be3cbc5634b7bebaf95842a10b3a85adb

--- product/device.mk
+++ product/device.mk
@@ -1,12 +1,3 @@
 # Gestures and Glove mode package
 PRODUCT_PACKAGES += \
     DeviceSettings
-
-# Device properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.device.chipset=Qualcomm MSM8960T Snapdragon S4 Pro \
-    ro.device.cpu=Dual-core 1.7 GHz Krait \
-    ro.device.gpu=Qualcomm Adreno 320 \
-    ro.device.rear_cam=8 MP \
-    ro.device.front_cam=VGA \
-    ro.device.screen_res=720X1280
