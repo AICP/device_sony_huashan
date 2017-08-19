LineageOS/android_device_sony_huashan

healthd: Add libcutils dependency for android_reboot
 * Required through 'libhealthd' for 'recovery'

Change-Id: Ibdf1a3a39081e8dd0d239b832e9c452a6f16a92f

healthd/Android.mk
@@ -7,6 +7,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp
 LOCAL_MODULE := libhealthd.board
+LOCAL_STATIC_LIBRARIES := libcutils
 LOCAL_C_INCLUDES := system/core/healthd/include
 LOCAL_CFLAGS := -Werror
 include $(BUILD_STATIC_LIBRARY)

AICP/device_sony_huashan

healthd/Android.mk
@@ -7,6 +7,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp
 LOCAL_MODULE := libhealthd.board
+LOCAL_WHOLE_STATIC_LIBRARIES := libcutils
 LOCAL_C_INCLUDES := system/core/healthd/include
 LOCAL_CFLAGS := -Werror
 include $(BUILD_STATIC_LIBRARY)